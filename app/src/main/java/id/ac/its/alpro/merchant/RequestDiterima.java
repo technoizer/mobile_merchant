4ef8d1bc4e7e95df5c299e1fa04e9ba841cc4e14
Luffi Aditya Sandy <luffi.as@gmail.com>
2016-02-18 17:16:41 +0700

    lagi

--- app/src/main/java/id/ac/its/alpro/merchant/RequestDiterima.java
+++ app/src/main/java/id/ac/its/alpro/merchant/RequestDiterima.java
@@ -1,5 +1,6 @@
 package id.ac.its.alpro.merchant;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -8,6 +9,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import java.util.Locale;
 
 public class RequestDiterima extends AppCompatActivity {
 
@@ -22,6 +25,18 @@ public class RequestDiterima extends AppCompatActivity {
     }
     public void callCustomer(View view){
         Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:082121212093"));
+        startActivity(intent);
+    }
+
+    public void getCustomerLocation(View view){
+
+        String map = "http://maps.google.co.in/maps?q=" + "Institut Teknologi Sepuluh Nopember";
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(map));
+        startActivity(intent);
+
+    }
+    public void makeReport(View view){
+        Intent intent = new Intent(this, LaporanService.class);
         startActivity(intent);
     }
 
